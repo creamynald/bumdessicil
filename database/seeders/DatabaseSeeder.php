8ca232c1d20b63ee2c8b7d099d374327b47b0e3c
rere <creamynald@gmail.com>
2024-07-08 17:44:23 +0700

    config beras seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use Database\Seeders\dataDemo\berasSeeder;
 use Database\Seeders\dataDemo\jenisBerasSeeder;
 use Illuminate\Database\Seeder;
 
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PermissionSeeder::class,
             jenisBerasSeeder::class,
+            berasSeeder::class,
         ]);
     }
 }
--- database/seeders/dataDemo/berasSeeder.php
+++ database/seeders/dataDemo/berasSeeder.php
@@ -12,6 +12,32 @@ class berasSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data_beras_demo = [
+            [
+                'jenis_beras_id' => 1,
+                'user_id' => 2,
+                'harga' => 20000,
+                'berat' => 12,
+                'deskripsi' => 'Beras merah yang sangat enak',
+            ],
+            [
+                'jenis_beras_id' => 2,
+                'user_id' => 3,
+                'harga' => 25000,
+                'berat' => 34,
+                'deskripsi' => 'Beras hitam yang sangat enak',
+            ],
+            [
+                'jenis_beras_id' => 3,
+                'user_id' => 4,
+                'harga' => 15000,
+                'berat' => 11,
+                'deskripsi' => 'Beras putih yang sangat enak',
+            ],
+        ];
+
+        foreach ($data_beras_demo as $beras) {
+            \App\Models\Beras::create($beras);
+        }
     }
 }
