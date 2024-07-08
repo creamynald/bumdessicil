c8f9beb80fa453ba8af3d5efbf839db9f6ceec81
rere <creamynald@gmail.com>
2024-07-08 17:40:36 +0700

    add beras seeder

---
+++ database/seeders/dataDemo/berasSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders\dataDemo;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class berasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
